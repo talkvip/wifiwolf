43a499a66173d0c2a27baee843bb0e8b99d3e542
lh-dasherz <lh@dasherz.net>
2014-11-20 17:34:25 +0800

    initial config setting

---
+++ src/main/java/net/dasherz/wifiwolf/wifidog/constant/WifidogConstants.java
@@ -0,0 +1,14 @@
+package net.dasherz.wifiwolf.wifidog.constant;
+
+public class WifidogConstants {
+
+	public static final String STAGE_COUNTERS="counters";
+	public static final String STAGE_LOGIN="login";
+	
+	public static final int AUTH_DENIED =0;
+	public static final int AUTH_VALIDATION_FAILED =6;
+	public static final int AUTH_ALLOWED =1;
+	public static final int AUTH_VALIDATION =5;
+	public static final int AUTH_ERROR =-1;
+	
+}
