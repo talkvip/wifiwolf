f73c818d0861619643f7a64ad3b67f7bf379dddc
lh-dasherz <lh@dasherz.net>
2014-11-21 17:06:04 +0800

    add test suit

---
+++ src/test/java/net/dasherz/wifiwolf/repository/RepositoryTest.java
@@ -0,0 +1,12 @@
+package net.dasherz.wifiwolf.repository;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ AuthPageRepositoryTest.class, AuthTypeRepositoryTest.class,
+		DictRepositoryTest.class, UserRepositoryTest.class })
+public class RepositoryTest {
+
+}
